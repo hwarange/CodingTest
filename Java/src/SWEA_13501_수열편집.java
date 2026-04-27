e0cfdd3160b9dbc3cc611d90c758373c0a614a35
hwarang <asdf78964@naver.com>
2026-04-27 17:53:46 +0900

    initial commit

---
+++ Java/src/SWEA_13501_수열편집.java
@@ -0,0 +1,45 @@
+
+class Node{
+	int val;
+	Node next;
+	
+	public Node(int val, Node next) {
+		this.val = val;
+		this.next = null;
+	}
+	
+}
+
+class LinkedList{
+	Node head;
+	
+	public void insert(int idx, int val) {
+		
+		Node newNode = new Node(val, null);
+		
+		
+		if(head == null) {
+			head = newNode;
+		}
+		
+		else if(idx == 0) {
+			
+		}
+	}
+}
+
+
+public class SWEA_13501_수열편집 {
+	
+	public static void main(String[] args) {
+		
+		
+		
+	}
+	
+	static void insert(int idx, int n) {
+		for(int i=0; i<=n; i++) {
+			
+		}
+	}
+}
