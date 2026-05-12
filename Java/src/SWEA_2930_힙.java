2a536030ea7dd5ad1c55d192e631f1a8f1f8ba3d
hwarang <asdf78964@naver.com>
2026-05-12 15:03:57 +0900

    [SWEA]2930_힙

---
+++ Java/src/SWEA_2930_힙.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class SWEA_2930_힙 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		int t = Integer.parseInt(br.readLine());
+		for(int tc=1; tc<=t; tc++) {
+//			sb.append("#").append(tc).append(" ");
+			System.out.print("#"+tc+" ");
+			
+			int n = Integer.parseInt(br.readLine());
+			
+			PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> {return o2 - o1;});
+			
+			for(int i=0; i<n; i++) {
+				st = new StringTokenizer(br.readLine());
+				int seq = Integer.parseInt(st.nextToken());
+				
+				if(seq == 1) {
+					int k = Integer.parseInt(st.nextToken());
+					pq.add(k);
+					continue;
+				}
+				
+				if(!pq.isEmpty()) {
+//					sb.append(pq.poll()).append(" ");
+					System.out.print(pq.poll()+" ");
+				}
+				else {
+//					sb.append(-1).append(" ");
+					System.out.print(-1+" ");
+					break;
+				}
+				
+			}
+//			sb.deleteCharAt(sb.length()-1).append("\n");
+			System.out.println();
+		}
+//		System.out.print(sb);
+	}
+}
