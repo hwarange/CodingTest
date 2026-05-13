cabd24f213c65f52e59eabb23f7aaa37c44a3f3a
hwarang <asdf78964@naver.com>
2026-05-13 17:56:17 +0900

    [SWEA] 4038 단어가 등장하는 횟수 최적화

---
+++ Java/src/SWEA_4038_단어가등장하는횟수_최적화.java
@@ -0,0 +1,83 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class SWEA_4038_단어가등장하는횟수_최적화 {
+	static int cnt;
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int t = Integer.parseInt(br.readLine());
+		for(int tc=1; tc<=t; tc++) {
+			sb.append("#").append(tc).append(" ");
+			char[] B = br.readLine().toCharArray();
+			char[] S = br.readLine().toCharArray();
+			
+			cnt = 0;
+			KMP(B, S);
+			sb.append(cnt).append("\n");
+		}
+		System.out.print(sb);
+	}
+	static int[] getPi(char[] pattern) {
+		int m = pattern.length;
+		int[] pi = new int[m];
+		int j=0; //접두사(Prefix)를 가리키는 포인터이자, 일치하는 길이
+		
+		// i는 접미사(Suffix)를 가리킴
+		for(int i=1; i<m; i++) {
+			
+			// 접두사와 접미사가 다른 경우 j를 이전의 일치했던 부분으로 되돌림
+			while(j > 0 && (pattern[i] != pattern[j])) {
+				j = pi[j-1];
+			}
+			
+			// 접두사와 접미사가 같은 경우 일치하는 길이가 늘어났으니 j를 키우고 pi에 기록
+			if(pattern[i] == pattern[j]) {
+				pi[i] = ++j;
+			}
+		}
+		
+		return pi;
+	}
+	
+	static void KMP(char[] text, char[] pattern) {
+		//pi만들기
+		int[] pi = getPi(pattern);
+		//문장의 전체 길이
+		int n = text.length;
+		//패턴의 길이
+		int m = pattern.length;
+		int j=0;	//패턴을 가리키는 포인터
+		
+		// i는 텍스트를 가리키면서 절대 뒤로 가지 않음(돌아가기X)
+		for(int i=0; i<n; i++) {
+			
+			//가다가 글자가 다른 경우 pi배열을 보고 j점프
+			while(j > 0 && text[i] != pattern[j]) {
+				j = pi[j-1];
+			}
+			
+			//글자가 같은 경우 j를 한 칸 전진
+			if(text[i] == pattern[j]) {
+				// j가 패턴 끝까지 도달한 경우? 찾음
+				if(j == m-1) {
+					cnt++;
+					// 또 패턴이 존재하는 경우가 있으니 다음 위치로 점프
+					j = pi[j];
+				}
+				
+				else j++;
+			}
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
