11a7beccd3b2281ba188d07f3ed59332546fc50e
hwarang <asdf78964@naver.com>
2026-05-19 11:46:14 +0900

    [PRO] kakao 파괴되지 않은 건물

--- Java/src/PRO_kakao_파괴되지않은건물.java
+++ Java/src/PRO_kakao_파괴되지않은건물.java
@@ -1,40 +1,74 @@
+import java.util.Arrays;
 
 public class PRO_kakao_파괴되지않은건물 {
-	public int solution(int[][] board, int[][] skill) {
+	public static int solution(int[][] board, int[][] skill) {
         int answer = 0;
         
-        for(int[] arr : skill) {
-        	
+        int[][] diff = new int[board.length+1][board[0].length+1];
+        
+        for(int[] arr: skill) {
         	int r1 = arr[1];
-        	int c1 = arr[2];
-        	int r2 = arr[3];
-        	int c2 = arr[4];
-        	
-        	int degree = arr[5];
-        	
+    		int c1 = arr[2];
+    		int r2 = arr[3];
+    		int c2 = arr[4];
+    		
         	if(arr[0] == 1) {
-        		for(int i=r1; i<=r2; i++) {
-        			for(int j=c1; j<=c2; j++) {
-        				board[i][j] -= degree;
-        			}
-        		}
+        		
+        		diff[r1][c1] -= arr[5];
+        		diff[r2+1][c2+1] -= arr[5]; 
+        		
+        		// 행기준 누적합
+        		diff[r1][c2+1] += arr[5];
+        		
+        		// 열기준 누적합
+        		diff[r2+1][c1] += arr[5];
+
         	}
-        	
         	else {
-        		for(int i=r1; i<=r2; i++) {
-        			for(int j=c1; j<=c2; j++) {
-        				board[i][j] += degree;
-        			}
-        		}
+        		
+        		diff[r1][c1] += arr[5];
+        		diff[r2+1][c2+1] += arr[5]; 
+        		
+        		// 행기준 누적합
+        		diff[r1][c2+1] -= arr[5];
+        		
+        		// 열기준 누적합
+        		diff[r2+1][c1] -= arr[5];
         	}
         }
         
+        //행기준
+        for(int i=0; i<diff.length; i++) {
+        	for(int j=1; j<diff[0].length; j++) {
+        		diff[i][j] += diff[i][j-1];
+        	}
+        }
+        
+        //열기준
+        for(int i=1; i<diff.length; i++) {
+        	for(int j=0; j<diff[0].length; j++) {
+        		diff[i][j] += diff[i-1][j];
+        	}
+        }
+
+        
         for(int i=0; i<board.length; i++) {
         	for(int j=0; j<board[0].length; j++) {
-        		if(board[i][j] > 0) answer++;
+        		
+        		diff[i][j] += board[i][j];
+        		
+        		if(diff[i][j] > 0) answer++;
         	}
         }
         
         return answer;
     }
+	
+//	public static void main(String[] args) {
+//		int[][] board = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+//		int[][] skill = {{1, 1, 1, 2, 2, 4}, {1, 0, 0, 1, 1, 2}, {2, 2, 0, 2, 0, 100}};
+//		
+//		System.out.println(solution(board, skill));
+//		
+//	}
 }
