9ffcfb863a077cf4fc6c5fac5da92f1f60561cd4
hwarange <asdf78964@naver.com>
2026-05-20 23:51:45 +0900

    [SWEA] 1461 프로세서 만들기 초반부

---
+++ Java/src/SWEA_1461_프로세서연결하기.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class SWEA_1461_프로세서연결하기 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+	}
+}
